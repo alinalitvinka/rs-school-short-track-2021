fd7383c5747bcfc226e2dedeabe2173c66f9d493
alinalitvinka <alinalitvinko7@gmail.com>
2021-04-09 15:56:13 +0300

    fix: fix 11th and 15th tasks solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -1,23 +1,12 @@
-/**
- * Given a string, return its encoding version.
- *
- * @param {String} str
- * @return {String}
- *
- * @example
- * For aabbbc should return 2a3bc
- *
- */
 function encodeLine(str) {
   let res = '';
   let counter = 1;
   for (let i = 0; i < str.length; i++) {
     if (str[i] === str[i + 1]) {
       counter++;
+    } else if (counter === 1) {
+      res += `${str[i]}`;
     } else {
-      if (counter === 1) {
-        res += `${str[i]}`;
-      }
       res += `${counter}${str[i]}`;
       counter = 1;
     }
