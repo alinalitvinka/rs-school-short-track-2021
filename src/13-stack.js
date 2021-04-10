29be83312ec56701e412fc66234854840f4fff34
alinalitvinka <alinalitvinko7@gmail.com>
2021-04-10 22:36:14 +0300

    feat: add 13th task solution

--- src/13-stack.js
+++ src/13-stack.js
@@ -1,26 +1,25 @@
-/**
- * Implement the Stack with a given interface via array.
- *
- * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
- */
 class Stack {
-  push(/* element */) {
-    throw new Error('Not implemented');
+  constructor() {
+    this.arr = [];
+    this.index = 0;
+  }
+
+  push(element) {
+    this.arr[this.index] = element;
+    this.index += 1;
   }
 
   pop() {
-    throw new Error('Not implemented');
+    if (this.arr[1]) {
+      const popEl = this.arr[this.index - 1];
+      this.arr = this.arr.slice(0, this.index - 1);
+      this.index -= 1;
+      return popEl;
+    } return undefined;
   }
 
   peek() {
-    throw new Error('Not implemented');
+    return this.arr[this.index - 1];
   }
 }
 
